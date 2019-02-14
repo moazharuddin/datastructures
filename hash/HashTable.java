642afe656a717043490ee65e8846782aaa33a912
moazharuddin <46071693+moazharuddin@users.noreply.github.com>
2019-02-14 10:30:36 +0530

    adding insert and find changes

--- hash/HashTable.java
+++ hash/HashTable.java
@@ -33,6 +33,50 @@ public class HashTable {
 		}
 		return true;
 	}
+	//returns preferred index position
+	public int hashFunc(String word){
+		int hashval = word.hashCode();
+		hashval = hashval % arrSize;
+		if(hashval < 0){
+			hashval += arrSize;
+		}
+		return hashval;
+	}
+	//return step size
+	public int stepSize(String word){
+		int hashval = word.hashCode();
+		hashval = hashval % arrSize;
+		if(hashval < 0){
+			hashval += arrSize;
+		}
+		return 3 - hashval % 3;
+	}
+	
+	public void insert(String word){
+		int hashVal = hashFunc(word);
+		int stepSize = stepSize(word);
+		
+		while(arr[hashVal] != null){
+			hashVal = hashVal + stepSize;
+			hashVal = hashVal % stepSize;
+		}
+		arr[hashVal] = word;
+		System.out.println("inserted word:" + word);
+		size++;
+	}
+	
+	public String find(String word){
+		int hashVal = hashFunc(word);
+		int stepSize = stepSize(word);
+		while(arr[hashVal] != null){
+			if(arr[hashVal].equals(word)){
+				return arr[hashVal];
+			}
+			hashVal = hashVal + stepSize;
+			hashVal = hashVal % stepSize;
+		}
+		return arr[hashVal];
+	}
 
 
 	public static void main(String[] args) {
